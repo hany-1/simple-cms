460a0ef531cf12c3689d29f1997b5a1dcf538973
hany-1 <hanyi7885@hotmail.com>
2024-05-14 00:50:33 +0800

    force scheme if not local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //if not local then force scheme to https
+        if (env('APP_ENV') != 'local') {
+            URL::forceScheme('https');
+        }
     }
 
     private function loadHelpers()
